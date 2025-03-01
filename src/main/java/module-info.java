60d97b49e6650d4c1556e821198a47f4dd228cbc
Collin Andrews <pizzaTrucks@outlook.com>
2025-03-01 00:50:24 -0600

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.audiotranscription {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.audiotranscription to javafx.fxml;
-    exports com.example.audiotranscription;
-}
